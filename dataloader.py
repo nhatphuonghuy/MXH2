b05ba9d5b5bbc2b68895ec2941f10978786e8e28
lexuan2602 <129180957+lexuan2602@users.noreply.github.com>
2025-03-27 13:05:18 +0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -27,6 +27,9 @@ import random
 
 dir='/data/zhihao/Bitcoin'#change to your own data dir
 data_dir='/data/zhihao/Bitcoin'#change to your own data dir
+
+data_dir='/data/EthereumS'#change to your own data dir
+
 def load_pickle(fname):
     with open(os.path.join(data_dir,fname), 'rb') as f:
         return pickle.load(f)
